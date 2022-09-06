a74e8a172d57dde048d87dda227bd18e9b388968
sheptalo <104015580+sheptalo@users.noreply.github.com>
2022-09-06 09:01:49 +0300

    Update ponchicPonchic.java
    b

--- src/sheptalo/ponchicPonchic.java
+++ src/sheptalo/ponchicPonchic.java
@@ -2,7 +2,7 @@ package sheptalo.ponchic.Ponchic;
 
 import mindustry.mod.Mod;
 
-public class ponchic.Ponchic extends Mod{
+public class ponchic.Ponchic extends Mod {
 
     @Override
     public void init(){
